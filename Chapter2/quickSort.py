a22aaa9907fdc591319eef268b3beb004329a1ae
unknown <Administrator@PC-201112312012.(none)>
2012-01-23 18:15:49 +0800

    modify quickSort.py

--- Chapter2/quickSort.py
+++ Chapter2/quickSort.py
@@ -1,22 +1,22 @@
 # quickSort.py
 
-def quickSort(arr,right,left):
-	if right >= left : return
-	ind = right
-	target = arr[left-1]
-	i=right
+def quickSort(arr,left,right):
+	if left >= right : return
+	ind = left
+	target = arr[right-1]
+	i=left
 	while True:
-		if i == left-1: break
+		if i == right-1: break
 		if arr[i] <= target:
 			tmp = arr[ind]
 			arr[ind] = arr[i]
 			arr[i] = tmp
 			ind += 1
 		i+=1
-	arr[left-1]=arr[ind]
+	arr[right-1]=arr[ind]
 	arr[ind]=target
-	quickSort(arr,right,ind)
-	quickSort(arr,ind+1,left)
+	quickSort(arr,left,ind)
+	quickSort(arr,ind+1,right)
 	
 if __name__ == '__main__':
 	arr = [2,1,19, 10, 11 , 19,100, 5 , 7]
